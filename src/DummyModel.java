065ac8b3f6a9795b84ddde22f246dac785cff195
Mackro Otterberg <spamiuso@gmail.com>
2013-04-24 15:16:46 +0200

    added gravity

--- src/DummyModel.java
+++ src/DummyModel.java
@@ -6,11 +6,12 @@ public class DummyModel implements IBouncingBallsModel {
 
 	private final double areaWidth;
 	private final double areaHeight;
+	private final double gravitation = -9.82;
 	private List<Ball> myBalls = new LinkedList<Ball>();
 
 	public DummyModel(double width, double height) {
 		myBalls.add(new Ball(450, 220, 50, 36, 50));
-		myBalls.add(new Ball(760, 360, 100, 300, 75));
+		myBalls.add(new Ball(760, 360, 100, -300, 75));
 		myBalls.add(new Ball(330, 125, 500, 500, 25));
 		this.areaWidth = width;
 		this.areaHeight = height;
@@ -24,24 +25,35 @@ public class DummyModel implements IBouncingBallsModel {
 			double y = myBalls.get(i).getY();
 			double velocityX = myBalls.get(i).getVelocityX();
 			double velocityY = myBalls.get(i).getVelocityY();
-			double radius = myBalls.get(i).getWidth();
+			double diameter = myBalls.get(i).getWidth();
+			
 
-			if (x < radius || x > areaWidth - radius) {
+			velocityY += gravitation;
+			myBalls.get(i).setVelocityY(velocityY);
+			
+			if ((x <= 0 && velocityX < 0)|| (x >= areaWidth - diameter && velocityX > 0)) {
 				velocityX *= -1;
 				myBalls.get(i).setVelocityX(velocityX);
+				System.out.println(velocityX + " ; " + velocityY);
 
 			}
-			if (y < radius || y > areaHeight - radius) {
+			if ((y <= 0 && velocityY < 0)|| (y >= areaHeight - diameter && velocityY > 0)) {
 				velocityY *= -1;
 				myBalls.get(i).setVelocityY(velocityY);
+				System.out.println(velocityX + " ; " + velocityY);
 			}
 
+			//Ball-to-Ball-Collision
 			for (int j = 0; j < myBalls.size(); j++) {
 				for (int n = 0; n < myBalls.size(); n++) {
 
 				}
 			}
 
+
+			velocityY += gravitation;
+			myBalls.get(i).setVelocityY(velocityY);
+			
 			myBalls.get(i).setX(x + velocityX * deltaT);
 			myBalls.get(i).setY(y + velocityY * deltaT);
 		}
